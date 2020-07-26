b56833552a4f4b320ae5c9e3d391a79d21472062
Nelson29101998 <65300138+Nelson29101998@users.noreply.github.com>
2020-07-26 19:17:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -316,22 +316,6 @@ $res = mysqli_query($conexionplanta, $sql);
           prevEl: '.swiper-button-prev',
         },
       });
-    } else if (md.table()) {
-      var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        slidesPerGroup: 2,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
     } else {
       var swiper = new Swiper('.swiper-container', {
         slidesPerView: 3,
