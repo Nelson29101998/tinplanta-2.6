3d9cb91607e7ba126cb46e6772d0bb932319d795
Nelson29101998 <65300138+Nelson29101998@users.noreply.github.com>
2020-08-02 16:10:54 -0400

    Add files via upload

---
+++ php/seasons.php
@@ -0,0 +1,22 @@
+<?php
+$meses = date("m");
+if($meses == 10){
+    //Halloween
+?>
+<img src="http://drive.google.com/uc?id=1IIhG_x-Z9Qu4wChpIpaFYikQsi3gYj8G"
+class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+<?php
+}else if($meses == 12){
+    //Navidad
+?>
+<img src="http://drive.google.com/uc?id=1lP8g4wAPNjIfssRYuwVHqiFi4dg_AqtZ"
+class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+<?php 
+}else{
+    //Normal
+?>
+<img src="http://drive.google.com/uc?id=16yHHkXWlqx5tzo21075pNeJpV1Xl1MIt" 
+class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+<?php
+}
+?>
