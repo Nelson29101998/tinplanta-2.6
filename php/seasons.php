19fae8b93ef8bf69bce1e48647a827da40462142
Nelson29101998 <65300138+Nelson29101998@users.noreply.github.com>
2020-08-03 10:15:43 -0400

    Update seasons.php

--- php/seasons.php
+++ php/seasons.php
@@ -1,22 +1,29 @@
 <?php
 $meses = date("m");
-if($meses == 10){
-    //Halloween
+if($meses == 4){
+    // * Pascua
 ?>
-<img src="http://drive.google.com/uc?id=1IIhG_x-Z9Qu4wChpIpaFYikQsi3gYj8G"
-class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+    <img src="http://drive.google.com/uc?id=1zbuAbr_rbpZFtOasDd7ywBm3zAhMtUyj" class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
 <?php
-}else if($meses == 12){
-    //Navidad
+}else if ($meses == 9) {
+    // * Fiesta Patria
 ?>
-<img src="http://drive.google.com/uc?id=1lP8g4wAPNjIfssRYuwVHqiFi4dg_AqtZ"
-class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
-<?php 
-}else{
-    //Normal
+    <img src="http://drive.google.com/uc?id=11WfoD6KiO_K13E5sZNoKuliWeVD5yHSt" class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+<?php
+} else if ($meses == 10) {
+    // * Halloween
 ?>
-<img src="http://drive.google.com/uc?id=16yHHkXWlqx5tzo21075pNeJpV1Xl1MIt" 
-class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+    <img src="http://drive.google.com/uc?id=1IIhG_x-Z9Qu4wChpIpaFYikQsi3gYj8G" class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+<?php
+} else if ($meses == 12) {
+    // * Navidad
+?>
+    <img src="http://drive.google.com/uc?id=1lP8g4wAPNjIfssRYuwVHqiFi4dg_AqtZ" class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
+<?php
+} else {
+    // * Normal
+?>
+    <img src="http://drive.google.com/uc?id=16yHHkXWlqx5tzo21075pNeJpV1Xl1MIt" class="animated fadeInDown delay-1s" alt="nomemp" id="nomemp" width="300">
 <?php
 }
 ?>
